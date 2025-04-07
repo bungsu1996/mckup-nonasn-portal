45306bc6a750b47907705af67b0be8e068db5563
bungsu1996 <venomous.zach@gmail.com>
2025-04-08 00:26:21 +0700

    remove path not used

--- app/routes.ts
+++ app/routes.ts
@@ -1,21 +1,9 @@
 import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
-  // index("routes/views/home.tsx"),
-  // index("routes/layout/home/HomeLayout.tsx"),
   layout("routes/layout/home/HomeLayout.tsx", [
     index("./routes/views/home/Index.tsx"),
-    route("profil", "./routes/views/profile/Index.tsx"),
-    // route("pengaturan", "./routes/pages/pengaturan.tsx"),
+    // route("profil", "./routes/views/profile/Index.tsx"), // children route untuk layout ini
   ]),
 
 ] satisfies RouteConfig;
-
-// const requireLogin = async ({ context, location }) => {
-//   if (!context.auth?.user) {
-//     throw redirectt({
-//       to: "/login",
-//       search: { redirect: location.href },
-//     });
-//   }
-// };
